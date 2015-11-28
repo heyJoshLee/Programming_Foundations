e28e3563747c9cc10f721be1c8a9a9ca09966204
jleewebdev <Josh>
2015-11-28 22:48:00 +0800

    Added scoring

--- rps.rb
+++ rps.rb
@@ -1,5 +1,11 @@
 VALID_CHOICES = %w(rock paper scissors lizard spock)
-VALID_CHOICES_SHORT = %w(r p sc l sp )
+VALID_CHOICES_SHORT = %w(r p sc l sp)
+player_score = 0
+computer_score = 0
+
+def game_over?(player_score, computer_score)
+  player_score >= 5 || computer_score >= 5
+end
 
 def prompt(message)
   puts "=> #{message}"
@@ -7,12 +13,14 @@ end
 
 loop do
   play_again = ""
+  choice = ""
 
-  choice = ''
   loop do
+    puts "---------------------------------------------"
     prompt "Choose one: #{VALID_CHOICES.join(', ')}"
     prompt "Please enter one of the following: "
     prompt VALID_CHOICES_SHORT.join(" ")
+    puts "---------------------------------------------"
     choice = gets.chomp.downcase
 
     choice = case choice
@@ -39,7 +47,7 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  puts "You chose: #{choice}; Computer chose #{computer_choice}"
+  puts "You chose: #{choice}; Computer chose #{computer_choice}\n"
 
   if    (choice == 'rock' && computer_choice == 'scissors') ||
         (choice == "rock" && computer_choice == "lizard") ||
@@ -51,7 +59,8 @@ loop do
         (choice == "lizard" && computer_choice == "paper") ||
         (choice == "spock" && computer_choice == "rock") ||
         (choice == "spock" && computer_choice == "scissors")
-    prompt "You won!"
+    prompt "You won!\n\n"
+    player_score += 1
   elsif (choice == 'scissors' && computer_choice == 'rock') ||
         (choice == "scissors" && computer_choice == "spock") ||
         (choice == "rock" && computer_choice == "paper") ||
@@ -62,17 +71,25 @@ loop do
         (choice == "lizard" && computer_choice == "rock") ||
         (choice == "spock" && computer_choice == "lizard") ||
         (choice == "spock" && computer_choice == "paper")
-    prompt "Computer won"
+    prompt "Computer won\n\n"
+    computer_score += 1
   else
-    prompt "Tie!"
+    prompt "Tie!\n\n"
   end
 
+  prompt "Player score: #{player_score}"
+  prompt "Computer score: #{computer_score}\n\n"
+
+if game_over? player_score, computer_score
   loop do
     puts "Would you like to play again?"
     puts "Y) Yes    |    N) No"
     play_again = gets.chomp.downcase
     break if %w(y n).include?(play_again)
   end
+  player_score = 0
+  computer_score = 0
+end
 
   break if play_again == "n"
 end
