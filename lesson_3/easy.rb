f9e7cee302d77672003c48bbb3ea299294601169
jleewebdev <lee45276@gmail.com>
2015-11-29 23:28:50 +0800

    tic_tac_toe initialize board

---
+++ lesson_3/easy.rb
@@ -0,0 +1,11 @@
+# Replace the word "important" with "urgent" in this string:
+
+# advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.sub! "important", "urgent"
+
+puts advice
+
+puts 42.between?(10, 100)
---
+++ lesson_3/tic_tac_toe.rb
@@ -0,0 +1,25 @@
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[4]}   |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[7]}   |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def intialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = 'X' }
+  new_board
+end
+
+board = intialize_board
+
+display_board(board)
